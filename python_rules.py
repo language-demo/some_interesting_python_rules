a0c47ebe664d7362e6719d692a6d2bf9ecc64a7b
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2025-12-12 11:50:09 +0000

    Update python_rules.py
    Added script content

--- python_rules.py
+++ python_rules.py
@@ -1,1 +1,42 @@
+import json
+from flask import Flask, make_response, request
+from flask_wtf import CSRFProtect
+from django.http import HttpResponseRedirect
 
+def update_and_show_counter(counter):
+    counter += 1
+
+    counter =+ 8
+    print(counter)
+
+
+counter = 10
+update_and_show_counter(counter)
+
+def complicated_code(input):
+    a=1
+    b=2
+    c=3
+    d=4
+    counter = 1
+
+    if a in (a,b,c,d): 
+        input += c
+        if a < b:
+            input += b
+            if c < d:
+                input += d
+                if a < c: 
+                    while counter < 10:
+                        input += a
+                        counter += 1
+                    if a < d:
+                        input += d
+                        if c < d:
+                            input += d
+                            if a < b:
+                                input += a
+
+    return print(input) 
+
+complicated_code("Input test")
